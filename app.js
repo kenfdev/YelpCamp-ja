9376af84bb7c3a854f1ff15cb1207d1a83cc0d2f
Ken Fukuyama <kenfdev@gmail.com>
2021-08-17 07:37:24 +0900

    Bootstrapで最低限のスタイルを適用

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
@@ -16,6 +17,7 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp',
 
 const app = express();
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
--- seeds/index.js
+++ seeds/index.js
@@ -12,16 +12,20 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp',
         console.log('MongoDBコネクションエラー！！！');
         console.log(err);
     });
-    
+
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const randomCityIndex = Math.floor(Math.random() * cities.length);
+        const price = Math.floor(Math.random() * 2000) + 1000;
         const camp = new Campground({
             location: `${cities[randomCityIndex].prefecture}${cities[randomCityIndex].city}`,
-            title: `${sample(descriptors)}・${sample(places)}`
+            title: `${sample(descriptors)}・${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: '木曾路はすべて山の中である。あるところは岨づたいに行く崖の道であり、あるところは数十間の深さに臨む木曾川の岸であり、あるところは山の尾をめぐる谷の入り口である。一筋の街道はこの深い森林地帯を貫いていた。東ざかいの桜沢から、西の十曲峠まで、木曾十一宿はこの街道に添うて、二十二里余にわたる長い谿谷の間に散在していた。道路の位置も幾たびか改まったもので、古道はいつのまにか深い山間に埋もれた。',
+            price
         });
         await camp.save();
     }
