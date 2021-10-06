3d7b04f0cff87ed1dffdd0b59f8d94f8032c89c5
Ken Fukuyama <kenfdev@gmail.com>
2021-10-06 11:10:31 +0900

    クラスターマップを一覧に追加

--- models/campground.js
+++ models/campground.js
@@ -12,6 +12,7 @@ imageSchema.virtual('thumbnail').get(function () {
     return this.url.replace('/upload', '/upload/w_200');
 });
 
+const opts = { toJSON: { virtuals: true } };
 const campgroundSchema = new Schema({
     title: String,
     images: [imageSchema],
@@ -38,7 +39,12 @@ const campgroundSchema = new Schema({
             type: Schema.Types.ObjectId,
             ref: 'Review'
         }
-    ]
+    ],
+}, opts);
+
+campgroundSchema.virtual('properties.popupMarkup').get(function () {
+    return `<strong><a href="/campgrounds/${this._id}">${this.title}</a></strong>
+    <p>${this.description.substring(0, 20)}...</p>`
 });
 
 campgroundSchema.post('findOneAndDelete', async function (doc) {
