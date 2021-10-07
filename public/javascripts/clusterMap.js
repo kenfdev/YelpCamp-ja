c702104432a12e7ae1037816634982d4d8d7e7a0
Ken Fukuyama <kenfdev@gmail.com>
2021-10-07 10:21:58 +0900

    全体的にスタイルを調整

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -1,12 +1,12 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-    container: 'map',
+    container: 'cluster-map',
     style: 'mapbox://styles/mapbox/light-v10',
     center: [138, 39],
     zoom: 3
 });
 
-console.log(campgrounds);
+map.addControl(new mapboxgl.NavigationControl());
 
 map.on('load', () => {
     // Add a new source from our GeoJSON data and
--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -6,6 +6,8 @@ const map = new mapboxgl.Map({
     zoom: 10 // starting zoom
 });
 
+map.addControl(new mapboxgl.NavigationControl());
+
 new mapboxgl.Marker()
     .setLngLat(campground.geometry.coordinates)
     .setPopup(
