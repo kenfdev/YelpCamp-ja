1f898d22093ed941f5019a176b09bf25d0e3e2f3
Ken Fukuyama <kenfdev@gmail.com>
2022-08-05 05:20:29 +0000

    create user on seed

--- seeds/index.js
+++ seeds/index.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const cities = require('./cities');
 const { descriptors, places } = require('./seedHelpers');
 const Campground = require('../models/campground');
+const User = require('../models/user');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp',
     { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
@@ -16,12 +17,15 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp',
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
+    await User.deleteMany({});
     await Campground.deleteMany({});
+    const user = new User({ username: 'test', email: 'test@test.test'});
+    const registeredUser = await User.register(user, 'test');
     for (let i = 0; i < 50; i++) {
         const randomCityIndex = Math.floor(Math.random() * cities.length);
         const price = Math.floor(Math.random() * 2000) + 1000;
         const camp = new Campground({
-            author: '6140028611461be5f79bb4c1',
+            author: registeredUser._id,
             location: `${cities[randomCityIndex].prefecture}${cities[randomCityIndex].city}`,
             title: `${sample(descriptors)}・${sample(places)}`,
             description: '木曾路はすべて山の中である。あるところは岨づたいに行く崖の道であり、あるところは数十間の深さに臨む木曾川の岸であり、あるところは山の尾をめぐる谷の入り口である。一筋の街道はこの深い森林地帯を貫いていた。東ざかいの桜沢から、西の十曲峠まで、木曾十一宿はこの街道に添うて、二十二里余にわたる長い谿谷の間に散在していた。道路の位置も幾たびか改まったもので、古道はいつのまにか深い山間に埋もれた。',
